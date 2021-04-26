689f572c1e1328370d68414754d1b1f56a062477
Yiming (Danny) Tang <70246398+ytang913@users.noreply.github.com>
2021-04-26 16:42:04 -0700

    adding code

---
+++ src/database.js
@@ -0,0 +1,40 @@
+let sys = require("sys"), fs = require("fs");
+let sqlite3 = require('sqlite3').verbose();
+
+const DBSOURCE = "pgccrimecollectorsearch.db" //name and create the database
+
+// If you use the TEXT storage class to store date and time value, you need to use the ISO8601 string format as follows:
+let db = new sqlite3.Database(DBSOURCE, (err) => {
+    if (err) {
+        // Cannot open database
+        console.error(err.message);
+        throw err;
+    } else {
+        console.log('Connected to the SQLite database.')
+        let install_sql = fs.readFileSync('./src/install_sqlite_tables.sql', 'utf-8').toString();
+        install_sql = install_sql.replace("\n", "");
+
+        const dataArr = install_sql.toString().split(');');
+        let x = 10;
+
+        db.serialize(() => {
+            db.run('PRAGMA foreign_keys=OFF;');
+            dataArr.forEach((query) => {
+                tempquery = query.replace(/(\r\n|\n|\r)/gm, "");
+                if (tempquery && (tempquery.toString().startsWith('--') === false)) {
+                    tempquery += ');';
+                    db.run(tempquery, err => {
+                        if (err) {
+                            console.log(err.message);
+                            throw err;
+                        }
+                    });
+                }
+            });
+        });
+
+    }
+    console.log("database loaded")
+});
+
+module.exports = db;
---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+let resultsRoute = require('./routes/results')
+let path = require('path')
+let bodyParser = require('body-parser')
+let db = require("./database.js")
+// Declare filesystem
+let fs        = require('fs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+  
+app.use(resultsRoute)
+
+app.use(express.static('public'))
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => console.info(`Server has started on ${PORT}`)) 
